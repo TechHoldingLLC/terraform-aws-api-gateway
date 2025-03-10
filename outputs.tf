d486d159d47ba11ccc17eb6ea48d548235ff13f9
Harshvardhansinh Jadeja <harshvardhan.jadeja@techholding.co>
2025-03-10 15:45:35 +0530

    fix: output change

--- outputs.tf
+++ outputs.tf
@@ -43,8 +43,9 @@ output "cloudfront_zone_id" {
 }
 
 output "cloudfront_domain_name" {
-  value = aws_api_gateway_domain_name.main.cloudfront_domain_name
+  value = try(aws_api_gateway_domain_name.main.cloudfront_domain_name ,null) 
 }
+
 output "regional_domain_name" {
-  value = aws_api_gateway_domain_name.main.regional_domain_name
+  value = try(aws_api_gateway_domain_name.main.regional_domain_name, null) 
 }
